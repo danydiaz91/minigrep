6993dfeeb8ae45f8fbccf0aa24106983052100e0
Dany Diaz <dany.diaz91@hotmail.com>
2020-07-01 01:14:52 -0500

    feat: spliting code into a library crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{env, fs, process};
-use std::error::Error;
+use std::env;
+use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,38 +10,8 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(error) = run(config) {
+    if let Err(error) = minigrep::run(config) {
         println!("Application error: {}", error);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Searching for '{}'", config.query);
-    println!("In file '{}'", config.filename);
-
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {query, filename})
     }
 }
